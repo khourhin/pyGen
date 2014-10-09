6a478f1988c148d4561d7fafd0374d5996f48003
khourhin <ekornobis@gmail.com>
2014-10-09 19:17:18 +0200

    adding stats depending of the type of input

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 #! /usr/bin/python
 import basics_fasta as bfas
+import format_check as fc
 
 #-------------------------------------------------------------------------------
 def fasta_stats(fasta, graphs_path=None):
@@ -7,6 +8,15 @@ def fasta_stats(fasta, graphs_path=None):
     seq_d = bfas.fasta_2_dict(fasta)
     bfas.make_summary(seq_d, graphs_path)
 
+#-------------------------------------------------------------------------------
+def file_stats(infile):
+    
+    if fc.isFasta(infile):
+        print infile + " looks like a fasta file"
+        fasta_stats(infile)
+    else:
+        raise IOError("Other formats than Fasta are not yet supported")
+        
 #-------------------------------------------------------------------------------
 
 if __name__ == "__main__":
@@ -18,4 +28,4 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     if args.infile:
-        fasta_stats(args.infile)
+        file_stats(args.infile)
