9b2b24d6b7796b105c27c8ff8d9ed08ca251ed90
khourhin <ekornobis@gmail.com>
2014-10-20 16:39:42 +0200

    adding blat and subsetting function

--- main.py
+++ main.py
@@ -2,12 +2,42 @@
 import format_check as fc
 import basics_fasta as bfas
 import basics_fastq as bfq
+import blasting as blst
+import common
+import argparse
+
+#-------------------------------------------------------------------------------
+def sample_fasta(fasta, nseq, outfile="default_out.fas"):
+    seq_d = bfas.fasta_2_dict(fasta)
+    seq_i = bfas.get_random_seqs(seq_d, nseq)
+    bfas.write_to_fas(seq_i, outfile)
+
+#-------------------------------------------------------------------------------
+def subset_fasta(fasta, ids_file, outfile="default_out.fas"):
+    """
+    Get a subset of the fasta file with only the sequence which ids are in 
+    ids_file.
+    """
+    seqids_l = common.get_lst_from_file(ids_file)
+    seq_d = bfas.fasta_2_dict(fasta)
+    seq_i = bfas.get_these_seqs(seq_d, seqid_l)
+    bfas.write_to_fas(seq_i, outfile)
 
 #-------------------------------------------------------------------------------
 def fasta_stats(fasta, graphs_path=None):
 
     seq_d = bfas.fasta_2_dict(fasta)
     bfas.make_summary(seq_d, graphs_path)
+
+#-------------------------------------------------------------------------------
+def blastN(query, db, outfile):
+    """
+    Perform a classical blastN of the query against the db. 
+    Query and db should be in fasta
+    outfile is the path to the blast output
+    """
+    blst.format_db(db)
+    blst.do_blastN(query, db, outfile)
 
 #-------------------------------------------------------------------------------
 def file_stats(infile):
@@ -28,11 +58,23 @@ def file_stats(infile):
 
 if __name__ == "__main__":
 
-    import argparse
+
+    # So far set by default:
+    BLAST_OUT = "blast_out.tab"
 
     parser = argparse.ArgumentParser(description='Unified Bioinformatician')
     parser.add_argument("infile", help="the input file")
+    parser.add_argument("-r","--random", type=int,
+                        help="Return a random sample of 'R' sequences  ")
+    parser.add_argument("-s", "--subset", help="Path to file with seqids to keep")
+    parser.add_argument("-bN","--blastNdb", help="A fasta file to blast against")
     args = parser.parse_args()
 
     if args.infile:
         file_stats(args.infile)
+
+        if args.random:
+            sample_fasta(args.infile, args.random)
+
+        if args.blastNdb:
+            blastN(args.infile, args.blastNdb, BLAST_OUT)
