154b1fa373387a3d382b8915cd8b939704ccc5ae
khourhin <ekornobis@gmail.com>
2014-10-20 16:00:07 +0200

    adding a library for easy blasting

---
+++ blasting.py
@@ -0,0 +1,27 @@
+import subprocess
+
+#-------------------------------------------------------------------------------
+def format_db(fasta):
+    """
+    Format a fasta file for having them as db for blast+
+    """
+
+    cmd = [PATH_BLAST + 'makeblastdb', '-dbtype', 'nucl', '-in', fasta ]
+    proc = subprocess.Popen( cmd, 
+                             stdout=subprocess.PIPE)
+    out, err = proc.communicate()
+
+    return out, err
+
+#-------------------------------------------------------------------------------
+def do_blast(query, db, outfile):
+    """
+    Launched a blast analysis for the [query] against the [db]
+    """
+    cmd = [PATH_BLAST + 'blastn', '-query', query, '-db', db, '-out', outfile,
+           '-outfmt', '6', '-evalue', '1e-6' ]
+    proc = subprocess.Popen( cmd, 
+                             stdout=subprocess.PIPE)
+    out, err = proc.communicate()
+
+
