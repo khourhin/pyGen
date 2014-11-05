6e53853b0c7d3e9bd95908f07c7554e52e8e6783
khourhin <ekornobis@gmail.com>
2014-11-05 13:36:46 +0100

    adding keggs parsing out of kaas server

--- work_on_b2go.py
+++ work_on_b2go.py
@@ -3,15 +3,12 @@
 import sys
 
 #-------------------------------------------------------------------------------
-def parse_b2go(annot_f):
+def parse_b2go(annot_f, annot_d):
     """
     Parse the annotation file out of b2go i.e ".annot"
     Return a seqids dict of dicts with:
     GOs, ECs and annot as keys
     """
-                        
-    annot_d = {}
-
     with open(annot_f, "r") as f:
 
         for line in f:
@@ -39,6 +36,27 @@ def parse_b2go(annot_f):
     return annot_d
 
 #-------------------------------------------------------------------------------
+def parse_KEGG(kaas_f, annot_d):
+    """
+    Parse the output of the KEGG annotation from the KAAS webserver
+    Return a annot dict
+    """
+
+    with open(kaas_f,"r") as f:
+
+        for line in f:
+            line = line.strip().split("\t")
+            
+            # If there is a kegg annotation (I think KEGGS should be unique)
+            if not len(line) == 1:
+                seqid = line[0]
+                kegg = line[1]
+                
+                annot_d[seqid]["KEGG"] = kegg
+                
+    return annot_d
+
+#-------------------------------------------------------------------------------
 def print_annot(annot_d):
     """
     Print to std out:
@@ -52,18 +70,23 @@ def print_annot(annot_d):
 
         GOs = ",".join(seq_d["GOs"])
         annot = ",".join(seq_d["annot"])
+        KEGG = seq_d["KEGG"]
         
         if "ECs" in seq_d:
             ECs = ",".join(seq_d["ECs"])
 
-        print "\t".join([seqid, annot, GOs, ECs])
+        print "\t".join([seqid, annot, KEGG, ECs, GOs])
 
 #-------------------------------------------------------------------------------
 # MAIN
 #-------------------------------------------------------------------------------
 if __name__ == "__main__":
-    
-    annot_d = parse_b2go(sys.argv[1])
+
+    # Create an empty annotation dict
+    annot_d = {}
+
+    annot_d = parse_b2go(sys.argv[1], annot_d)
+#    annot_d = parse_KEGG(sys.argv[2], annot_d)
     print_annot(annot_d)
     
     
