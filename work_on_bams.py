b836e3410abd9ca1c92cbc808aa29f78d2834481
khourhin <ekornobis@gmail.com>
2015-03-25 15:10:26 +0100

    working coverage estimates

--- work_on_bams.py
+++ work_on_bams.py
@@ -16,21 +16,38 @@ def getLocus(bam, chromo, start, end):
 
 
 #-------------------------------------------------------------------------------
-def getCoverage(bam):
+def getCoverage(bam, bedout):
     """
     Get coverage for each genes using bedtools
+    Create an histogram using bedtools and then print to stdout
+    the average coverage for each genes/transcripts
     """
     log.info("Getting coverage for: %s" % bam)
 
-    command = [ "bedtools", "genomecov", "-ibam", bam ]
-    proc = subprocess.Popen(command)
-    proc.communicate()
-    print "ah"
+    # Get coverage histogram from bedtools
+    with open(bedout, "w") as fout:
     
+        command = [ "bedtools", "genomecov", "-ibam", bam ]
+        proc = subprocess.Popen(command, stdout=subprocess.PIPE)
+        fout.write( proc.communicate()[0] )
 
+    with open(bedout, "r") as fin:
+        cov_d = {}
+        for entry in fin:
+            entry = entry.split("\t")
+            ref_seq = entry[0]
+
+            cov_d[ref_seq] = cov_d.setdefault(ref_seq, 0) \
+                             + int(entry[1]) * int(entry[2]) / float(entry[3])
+
+    for k in cov_d:
+        print "%s\t%0.1f" % (k, cov_d[k])
+        
 #-------------------------------------------------------------------------------
 if __name__ == "__main__":
 
+    bedout = "coverage_from_bed.tab"
+    
     #getLocus(sys.argv[1],1,1,100)
-    getCoverage(sys.argv[1])
+    getCoverage(sys.argv[1], bedout)
     
