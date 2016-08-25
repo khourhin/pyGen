c060f9ffb9dea4e446089b840a3f58196172c358
Etienne Kornobis <ekornobis@gmail.com>
2016-08-25 17:48:20 +0200

    Alternative splicing analysis with DEXSEq and JunctionSeq

---
+++ R/DEXSeq.R
@@ -0,0 +1,70 @@
+                                        # REQUIREMENTS:
+## meta_sample.tab:
+## col1: sample names, col2 NAMED "condition" ("condition" string is used in model parameters later on)
+## SEEMS LIKE SAMPLE NAMES SHOULD BE IN THE SAME ORDER AS countFiles !!!
+
+suppressPackageStartupMessages(library( "DEXSeq" ))
+pdf()
+
+# Threads
+NTHREADS = MulticoreParam(workers=30)
+
+## TO EDIT
+countFiles <- list.files("../dexseq/counts_considered_stranded", full.names=TRUE)
+flattenedFile <- "../dexseq/hg19_ensembl_4DEXSeq.gff"
+metaSample <- read.table("../dexseq/meta_sample.tab")
+##
+
+message("Count files used:")
+print(countFiles)
+message("MetaData:")
+print(metaSample)
+message("GFF flattened file used:")
+print(flattenedFile)
+
+message("Importing data ...")
+dxd <- DEXSeqDataSetFromHTSeq(
+    countFiles,
+    sampleData=metaSample,
+    design= ~ sample + exon + condition:exon,
+    flattenedfile=flattenedFile
+)
+
+head(colData(dxd))
+head(counts(dxd))
+
+message("Show how preceding columns are separated according
+ to counts in 'this' exons and counts in 'other' exons:")
+split(seq_len(ncol(dxd)), colData(dxd)$exon)
+
+message("See the vignette for more exploring commands ...")
+
+## Normalization (same as DESeq, DESeq2)
+message("Estimating size factors ...")
+dxd <- estimateSizeFactors(dxd)
+message("Estimating dispersions ...")
+dxd <- estimateDispersions(dxd, BPPARAM=NTHREADS)
+
+## per-exon dispersion estimates versus the mean normalised count
+plotDispEsts(dxd)
+
+## Perform test if condition effect
+message("Testing DEU ...")
+dxd <- testForDEU(dxd, BPPARAM=NTHREADS)
+
+message("Calculate fold change ...")
+dxd <- estimateExonFoldChanges(dxd, fitExpToVar="condition", BPPARAM=NTHREADS)
+
+dxr1 <- DEXSeqResults(dxd)
+print(dxr1)
+
+## Number of significant exons? (called genes in vignette) with false discovery rate of 5%
+table(dxr1$padj < 0.05)
+
+## How many genes are affected
+table(tapply(dxr1$padj<0.05, dxr1$groupID, any))
+
+## Get genes with DR exons
+which(tapply(dxr1$padj<0.05, dxr1$groupID, any))
+
+dev.off()
---
+++ R/JunctionSeq.R
@@ -0,0 +1,46 @@
+suppressPackageStartupMessages(library("JunctionSeq"))
+
+juncSeqPipe <- function(sampleMeta, gffFile, countFolder, outPrefix){
+
+    sampleMeta <- read.table(sampleMeta, header=TRUE, stringsAsFactors=FALSE)
+    countFiles <- list.files(countFolder)
+
+    jscs <- runJunctionSeqAnalyses(sample.files = countFiles,
+                                   sample.names = sampleMeta[,1],
+                                   condition=factor(sampleMeta[,2]),
+                                   flat.gff.file = gffFile,
+                                   nCores = 1,
+                                   analysis.type = "junctionsAndExons" )
+
+    message("Writing results...")
+    writeCompleteResults(jscs,outfile.prefix = outPrefix, save.jscs = TRUE)
+
+    message("Plotting results...")
+    buildAllPlots(jscs=jscs, outfile.prefix = paste(outPrefix, "plots", sep="/"),
+                  use.plotting.device = "png", FDR.threshold = 0.01 )
+}
+
+##-------------------------------------------------------------------------------
+                                        # MAIN
+##-------------------------------------------------------------------------------
+message("Starting JunctionSeq analysis...")
+
+args = commandArgs(trailingOnly = TRUE)
+juncSeqPipe(args[1], args[2], args[3], args[4])
+
+##-------------------------------------------------------------------------------
+                                        # USAGE
+##-------------------------------------------------------------------------------
+
+## $1: Table with headers:
+## col1: sample names(same order as in countFolder)
+## col2: conditions
+## $2: Flattened (see JunctionSeq manual) GFF file
+## $3: Path to folder with counts (see JunctionSeq for counting instructions)
+## The path SHOULD NOT end up with "/"
+## $4: Prefix for the results
+
+##-------------------------------------------------------------------------------
+                                        # EXAMPLE
+##-------------------------------------------------------------------------------
+
